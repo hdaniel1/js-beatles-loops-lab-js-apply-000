752244e9c541bb6ea55c37f9b631f3fdb8155407
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-17 03:59:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (i = 0; i = musicians.length - 1; i++) {
-    array.push(`${musicans[i] plays ${instruments[i]}`)
+    array.push(`${musicans[i]} plays ${instruments[i]}`)
   }
   return array
 }
