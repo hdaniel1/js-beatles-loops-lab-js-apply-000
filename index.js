e285a4baff130a148c162d3935de83198cb1d21b
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-17 04:08:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var i = 0
   while (facts[i]) {
-    facts[i] = i + '!!!'
+    facts[i] = facts[i] + '!!!'
     i++
   }
   return facts
