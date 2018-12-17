93d668cf44dab6ca5a6f2f1a21a3cb3f21031879
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-17 03:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (i = 0; i = musicians.length - 1; i++) {
+    array.push(`${musicans[i] plays ${instruments[]}`)
+  }
+  return array
+}
